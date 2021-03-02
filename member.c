3c565a4439e08ee801ae3e29a4cb9a35680d3104
Park-yu-su <pys01022342867@gmail.com>
2021-03-02 18:05:19 +0900

    회원가입, 로그인 기능 및 관리자 기능 구현

--- member.c
+++ member.c
@@ -16,23 +16,25 @@ typedef struct Info{
 
 } Info;
 
-
+//회원가입 함수
 void inputInfo(){
-	FILE *fp = fopen("info.txt","ab+");
+	FILE *fp = fopen("info.txt", "ab+");
 	Info input, person_list[100];
-	int N = 0, read_check; //갖고 올 회원 수
+	int N = 0, noone_check = 0; //갖고 올 회원 수, 회원이 없을 때 회원번호 판별 변수
 
 	rewind(fp); //회원 리스트에다가 기존 회원의 정보들 삽입
-	while(fread(&person_list[N], sizeof(Info),1,fp)){ 
-		N+=1;
+	while(fread(&person_list[N], sizeof(Info), 1, fp)){ 
+		N += 1;
+		noone_check = 1;
 	}
+	// person_list 0~N-1에는 회원 정보가 담겨있다!
 
 	while(1){
 		int isExist = 0;
 		printf("사용하실 ID를 입력하세요 : ");
 		scanf("%s", input.id);
 
-		for(int i = 0; i <= N; i++){
+		for(int i = 0; i < N; i++){
 			if(strcmp(input.id, person_list[i].id) == 0){ //만약 닉네임이 겹치면
 				printf("겹치는 ID가 존재합니다. 다시 입력해주세요\n");
 				isExist = 1;
@@ -54,7 +56,7 @@ void inputInfo(){
 		int isExist = 0;
 		printf("사용하실 닉네임을 입력하세요 : ");
 		scanf("%s", input.nickname);
-		for(int i = 0; i <= N; i++){
+		for(int i = 0; i < N; i++){
 			if(strcmp(input.id, person_list[i].id) == 0){ //만약 닉네임이 겹치면
 				printf("겹치는 ID가 존재합니다. 다시 입력해주세요\n");
 				isExist = 1;
@@ -66,35 +68,39 @@ void inputInfo(){
 	}
 
 	printf("관리자 계정으로 생성하시겠습니까? (1/0) : ");
-	scanf("%d",&input.manage);
-	
-	input.member_number = N+1;
+	scanf("%d", &input.manage);
+
+	if(noone_check == 0) //아무도 없는 경우 회원번호
+		input.member_number = N + 1;
+	else
+		input.member_number = person_list[N-1].member_number + 1;
+
 
 	fseek(fp,0,SEEK_END); //맨 끝으로 이동
-	fwrite(&input,sizeof(Info),1,fp); //이진 파일로 저장
+	fwrite(&input, sizeof(Info), 1, fp); //이진 파일로 저장
 	printf("\n회원가입이 완료되었습니다!\n");
 	fclose(fp);
 
 }
 
 
-
-Info login(){	
-	FILE *fp = fopen("info.txt","ab+");
+//로그인 함수
+Info login(){
+	FILE *fp = fopen("info.txt", "ab+");
 	Info person_list[100];
 	char check_id[30], check_password[30];
 	int isExist = 0, N = 0, me; //로그인 닉네임 체크, 갖고 올 회원 수, 몇 번째 회원
 
 	rewind(fp); //회원 리스트에다가 기존 회원의 정보들 삽입
 	while(fread(&person_list[N], sizeof(Info), 1, fp)){ 
-		N+=1;
-		//	fread(&person_list[N],sizeof(Info),1,fp);
+		N += 1;
 	}
+
 	while(1){
 		int isokay = 0;
 		printf("ID : "); //ID 로그인 과정
-		scanf("%s",check_id);
-		for(int i = 0; i <= N; i++){
+		scanf("%s", check_id);
+		for(int i = 0; i < N; i++){
 			if(strcmp(check_id, person_list[i].id) == 0){ //만약 닉네임이 겹치면
 				isExist = 1;
 				me = i; // 해당 번째 인물 위치 저장
@@ -113,7 +119,7 @@ Info login(){
 
 		printf("Password : ");
 		scanf("%s", check_password);
-		if(strcmp(check_password, person_list[me].password)==0){
+		if(strcmp(check_password, person_list[me].password) == 0){
 			printf("\n로그인이 성공했습니다.\n");
 			break;
 		}
@@ -127,6 +133,7 @@ Info login(){
 	return person_list[me];
 }
 
+//ID랑 PW를 찾는 함수
 void searchIdPass(int check){
 	FILE *fp = fopen("info.txt","ab+");
 	Info person_list[100];
@@ -134,24 +141,24 @@ void searchIdPass(int check){
 	char input_info[5][30]; // 0:ID 1:이름 2:닉네임 3:이메일 4:번호
 	rewind(fp);
 
-	while(fread(&person_list[N], sizeof(Info),1,fp)){ 
-		N+=1;
+	while(fread(&person_list[N], sizeof(Info), 1, fp)){ 
+		N += 1;
 	}
 
 	if(check==1){ //ID 찾기
 		printf("ID를 찾기 위한 정보를 입력하세요\n");
 		printf("이름 : ");
-		scanf("%s",input_info[1]);
+		scanf("%s", input_info[1]);
 		printf("E-mail : ");
-		scanf("%s",input_info[3]);
+		scanf("%s", input_info[3]);
 		printf("번호(휴대폰 - 없이) : ");
-		scanf("%s",input_info[4]);
+		scanf("%s", input_info[4]);
 
-		for(int i = 0; i <= N; i++){
+		for(int i = 0; i < N; i++){
 			if(strcmp(person_list[i].name, input_info[1]) == 0 && strcmp(person_list[i].email, input_info[3]) == 0 && strcmp(person_list[i].phone, input_info[4]) == 0){
 				idExist = 1;
 				printf("\n입력하신 정보와 일치하는 ID를 찾았습니다.\n");
-				printf("ID : %s\n",person_list[i].id);
+				printf("ID : %s\n", person_list[i].id);
 				break;
 			}
 		}
@@ -163,7 +170,7 @@ void searchIdPass(int check){
 
 		printf("ID를 입력하세요 : ");
 		scanf("%s",input_info[0]);
-		for(int i = 0; i <= N; i++){
+		for(int i = 0; i < N; i++){
 			if(strcmp(person_list[i].id, input_info[0]) == 0){
 				idExist = 1;
 				M = i;
@@ -175,15 +182,15 @@ void searchIdPass(int check){
 		else{
 			printf("비밀번호를 찾기 위한 정보를 입력하세요\n");
 			printf("이름 : ");
-			scanf("%s",input_info[1]);
+			scanf("%s", input_info[1]);
 			printf("E-mail : ");
-			scanf("%s",input_info[3]);
+			scanf("%s", input_info[3]);
 			printf("번호(휴대폰 - 없이) : ");
 			scanf("%s",input_info[4]);
 
 			if(strcmp(person_list[M].name, input_info[1]) == 0 && strcmp(person_list[M].email, input_info[3]) == 0 && strcmp(person_list[M].phone, input_info[4]) == 0){
 				printf("\n입력하신 정보와 일치하는 비밀번호를 찾았습니다.\n");
-				printf("비밀번호 : %s",person_list[M].password);
+				printf("비밀번호 : %s", person_list[M].password);
 			}
 			else
 				printf("\n입력하신 정보와 일치하는 비밀번호가 존재하지 않습니다.\n");
@@ -193,10 +200,11 @@ void searchIdPass(int check){
 	fclose(fp);
 }
 
+//관리자 메뉴 함수
 void manageMember(){	
 	FILE *fp = fopen("info.txt","ab+");
 	Info person_list[100];
-	int N = 0, go, input_memnum;
+	int N = 0, go, input_memnum, delete; //최대 인원, 이동, 입력할 회원번호, 삭제할 회원 순서
 	rewind(fp);
 	while(fread(&person_list[N], sizeof(Info),1,fp)){ 
 		N+=1;
@@ -211,28 +219,51 @@ void manageMember(){
 			for(int i = 0; i < N; i++)
 				printf("%02d| 회원번호: %d / 이름: %s / 닉네임: %s / ID: %s / PW: %s\n", i+1,person_list[i].member_number, person_list[i].name, person_list[i].nickname, person_list[i].id, person_list[i].password);
 		}
+
 		else if(go == 2){ // 
 			printf("삭제하실 회원의 회원번호를 입력하세요 : ");
-			scanf("%d",&input_memnum);
+			scanf("%d", &input_memnum);
 			for(int i = 0; i <= N; i++){
-				if(person_list[i].member_number == input_memnum)
+				if(person_list[i].member_number == input_memnum){
+					delete = i;
 					break;
+				}
 			}
+			printf("회원번호 %d번 \"%s\" 고객님의 정보를 삭제하시겠습니까? (1/0) : ", person_list[delete].member_number, person_list[delete].name);
+			scanf("%d", &go);
+			if(go == 0)
+				printf("삭제를 취소합니다.\n");
+			else{
+				for(int i = delete; i <= N; i++)
+					person_list[i] = person_list[i+1]; //덮어쓰기
+				N -= 1;
+				fclose(fp);
+				fp = fopen("info.txt", "wb");
+				rewind(fp);
+				for(int i = 0; i < N; i++)
+					fwrite(&person_list[i], sizeof(Info), 1, fp);
+				printf("삭제가 완료되었습니다.\n");
+				fclose(fp);
+			}
+
+
 		}
 		else
 			break;
 	}
+	fclose(fp);
 }
 
+// 메뉴창을 프린트하는 함수
 int printMenu(Info member){
 	int go; 
 	if(member.manage == 1){ //관리자
 		printf("\n===============================================\n");
 		printf("1. 로그인 / 2. 회원가입 / 3. 로그인 정보 출력 / ");
 		printf("4. ID 찾기 / 5. 비밀번호 찾기 / 6. 로그아웃 / 7. 종료\n");
-		printf("관리자 메뉴 [ 8. 회원 관리 / 9. 승률 패치 ]\n");
+		printf("관리자 메뉴 [ 8. 회원 관리 ]\n");
 		printf("입력 : ");
-		scanf("%d",&go);
+		scanf("%d", &go);
 
 	}
 	else{
@@ -240,7 +271,7 @@ int printMenu(Info member){
 		printf("1. 로그인 / 2. 회원가입 / 3. 로그인 정보 출력 / ");
 		printf("4. ID 찾기 / 5. 비밀번호 찾기 / 6. 로그아웃 / 7. 종료\n");
 		printf("입력 : ");
-		scanf("%d",&go);
+		scanf("%d", &go);
 
 	}
 	return go;
@@ -263,12 +294,10 @@ int main(){
 			if(login_check == 0)
 				printf("로그인이 필요합니다.\n");
 			else{
-				printf("\nID: %s\n", member.id);
-				printf("Password: %s\n", member.password);
 				printf("Name: %s\n", member.name);
+				printf("NickName: %s\n", member.nickname);
 				printf("E-mail: %s\n", member.email);
 				printf("Phone-number: %s\n", member.phone);
-				printf("NickName: %s\n", member.nickname);
 				printf("Member_number: %d\n", member.member_number);
 			}
 		}	
@@ -286,14 +315,11 @@ int main(){
 		else if(go == 8 && member.manage == 1){
 			manageMember();
 		}
-		else if(go == 9 && member.manage == 1){
-			break; //아직 구현X
-		}
 		else 
 			break;
 		go = printMenu(member);
 
-	}
+}
 
 
 
