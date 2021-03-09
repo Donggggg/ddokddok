12aa51410cd919c5bb45e9af042c72dbdb25d1c9
heungsu0101 <heungsu0101@naver.com>
2021-03-09 16:09:38 +0900

    설정 및 명예의전당 머지

--- main.c
+++ main.c
@@ -6,12 +6,20 @@
 #include "member.h"
 #include "sudoku.h"
 
+#include "score.h"
+#include "setting.h"
+//#include "back_g.h"
+
 void soloMode();
 void multiMode();
 
 int main()
 {
 	int input;
+
+	Game *game = malloc(sizeof(Game)); //멀티모드 진행 게임 상황
+	Setting *set = malloc(sizeof(Setting)); //설정 저장
+
 	Info *player = malloc(sizeof(Info)); 
 	startLogin(player); //로그인 함수
 	
@@ -27,8 +35,10 @@ int main()
 			multiMode();
 			break;
 		case 3 : // 명예의 전당
+			printScore();
 			break;
 		case 4 : // 설정
+			setOption(&set); 		//인자 포인터?
 			break;
 		case 5 : // 로그아웃
 			logout(player);
@@ -69,7 +79,7 @@ void soloMode()
 			printf("점수를 등록하시겠습니까? (1/0)");
 			scanf("%d",&regist);
 			if(regist == 1){
-				input_score(player, times, level, input);
+				inputScore(player, times, level, input);
 			}
 
 			break;
@@ -85,7 +95,7 @@ void soloMode()
 			printf("점수를 등록하시겠습니까? (1/0)");
 			scanf("%d",&regist);
 			if(regist == 1){
-			
+				inputScore(player, times, level, input);
 			}
 
 
@@ -100,5 +110,13 @@ void soloMode()
 
 void multiMode()
 {
+	int road_check = roadGame(&game);	//인자 포인터?
+	if(road_check == 1){
+		//세이브파일 존재 : game에는 이전 게임 정보가 삽입
+	}
+	else{
+		setGame(&game, &set);	//인자 포인터?
+	}
+
 	startMaze(MULTI,level);
 }
